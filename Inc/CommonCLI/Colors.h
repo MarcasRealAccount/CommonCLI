4d32e6c93bdb44ec2eae050dff37d166603b7377
MarcasRealAccount <thetremine.lumicks@gmail.com>
2021-11-24 09:38:59 +0100

    Add Success color

--- Inc/CommonCLI/Colors.h
+++ Inc/CommonCLI/Colors.h
@@ -6,6 +6,7 @@ namespace CommonCLI::Colors {
 	static constexpr ANSI::GraphicsForegroundColor Info      = { 127, 255, 255 };
 	static constexpr ANSI::GraphicsForegroundColor Warn      = { 255, 127, 0 };
 	static constexpr ANSI::GraphicsForegroundColor Error     = { 255, 30, 30 };
+	static constexpr ANSI::GraphicsForegroundColor Success   = { 30, 255, 30 };
 	static constexpr ANSI::GraphicsForegroundColor Usage     = { 127, 255, 63 };
 	static constexpr ANSI::GraphicsForegroundColor Help      = { 127, 255, 255 };
 	static constexpr ANSI::GraphicsForegroundColor Arg       = { 255, 255, 66 };
